fe1b1f57dcebf8221938b6dddd8f672d974206a5
Temz <temitopeabiodun685@gmail.com>
2023-11-07 21:37:02 +0000

    adjusted pycodestyle

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,10 +25,12 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', strict_slashes=False)
 def abort_unathorized_request():
     """Abort request for error 401"""
     abort(401)
+
 
 @app_views.route('/forbidden', strict_slashes=False)
 def abort_no_resource():
